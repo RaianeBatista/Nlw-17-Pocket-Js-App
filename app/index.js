f7abd3a663b7e5cae2b87950f5ce44753c343f2d
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2024-09-11 11:25:00 -0300

    adiciona metas abertas

--- app/index.js
+++ app/index.js
@@ -26,9 +26,9 @@ const listarMetas = async () => {
     instructions: false,
   });
 
-    metas.forEach((m) => {
-      m.checked = false;
-    });
+  metas.forEach((m) => {
+    m.checked = false;
+  });
 
   if (respostas.length == 0) {
     console.log('Nenhuma meta selecionada!');
@@ -57,8 +57,25 @@ const metasRealizadas = async () => {
   }
 
   await select({
-    message: 'Metas Realizadas',
+    message: 'Metas Realizadas: total =  ' + realizadas.length,
     choices: [...realizadas],
+  });
+};
+
+//agua[] - caminhar [] - cantar [x], se não tiver marcado vai p lista de metas abertas
+const metasAbertas = async () => {
+  const abertas = metas.filter((meta) => {
+    return !meta.checked; //   return meta.checked != true;
+  });
+
+  if (abertas.length == 0) {
+    console.log('Não existem metas abertas! :)');
+    return;
+  }
+
+  await select({
+    message: 'Metas Abertas: total = ' + abertas.length,
+    choices: [...abertas],
   });
 };
 
@@ -80,6 +97,10 @@ const start = async () => {
           value: 'realizadas',
         },
         {
+          name: 'Metas abertas',
+          value: 'abertas',
+        },
+        {
           name: 'Sair',
           value: 'sair',
         },
@@ -96,6 +117,9 @@ const start = async () => {
         break;
       case 'realizadas':
         await metasRealizadas();
+        break;
+      case 'abertas':
+        await metasAbertas();
         break;
       case 'sair':
         console.log('Até a próxima!');
--- app/todo.md
+++ app/todo.md
@@ -1,7 +1,7 @@
 - [x] Criar metas
 - [x] Listar metas:
 - [x] Metas realizadas
-      Metas abertas
+- [x]   Metas abertas
 - [x] Mascar/Desmarcar metas como concluídas
       -Remover metas
       -Sistema de mensagens
